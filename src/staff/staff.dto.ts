7ff50f60e378cc3bc2aa3a518eeba7d0f5dc491a
VAL099 <valeriy099@yahoo.com>
2024-05-26 19:40:08 +0300

    New Models

--- src/staff/staff.dto.ts
+++ src/staff/staff.dto.ts
@@ -1,10 +1,16 @@
-import { IsPositive, IsString, IsOptional, IsDate } from "class-validator";
+import { IsPositive, IsString, IsOptional, Length, Matches } from "class-validator";
 
 export class StaffDto{
     @IsString()
     readonly name: string;
 
     @IsString()
+    // @Length(13, 13)
+    @Matches(/^\d{13}$/, { message: 'IDNP must be a 13-digit number' })
+    readonly idnp: string;
+
+    @IsString()
+    @Matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, { message: 'Email is invalid' })
     readonly email: string;
 
     @IsPositive()
